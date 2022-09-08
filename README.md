186c6fe4bc149909fc28a826c5786c1a78a9a6fd
Christian <cmmorgado53@gmail.com>
2022-09-08 13:09:19 -0400

    SI Sessions 1_2 and 2_2 added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# SI_Session
+# SI Session files are organized like this:
+# SI_Session1_2 = Week 1 Sesh 2
+# SI_Session2_1 = Week 2 Sesh 1
---
+++ SI_Session1_2.py
@@ -0,0 +1,31 @@
+"""
+SI Session Notes for the second session of the first week
+"""
+
+def printtest():
+    """
+    This function showcases the sep and end commands
+    """
+    # Sep Command
+    print("\n", "Christian", "Jackson")
+    # This puts a space between the two strings because the separater always separates the concatenation with a space
+    print("Abe", "Christian", "Irwin", sep = "Jackson", end = "")
+
+
+    print("\n")
+
+    # End Command
+    print("Abe")
+    print("Christian")
+    # This creates a new line because \n is at the end of every command
+    print("Abe", end = "poop")
+    print("Christian")
+
+# def printvariables():
+
+
+
+
+
+
+printtest()
