c0ae0bdc9b49e3b9303b72568c73e4f30bec14fd
Christian <cmmorgado53@gmail.com>
2022-10-16 16:21:39 -0400

    wiuefbowgvwegfbowebvowehoweh

--- test_review.py
+++ test_review.py
@@ -9,6 +9,12 @@
 #note - thing.isdigit() will determine if a string can be an int or not
 #prerequisite- there are always 100 numbers in the file
 
+#write a recursive function that counts down from a given value and prints the number. 
+#Example, given the number 10, the function would print 10 9 8 7 6 5 4 3 2 1.
+
+import arrays
+import array_utils
+
 """
 Question 1
 """
@@ -21,7 +27,33 @@ def getNumbers(filename):
             for value in split_line:
                 if value.isdigit() == True:
                     count += 1
+        
+        abe_array = arrays.Array(count)
+        array_index = 0
+
+        for line in file:
+            stripped_line = line.strip()
+            split_line = stripped_line.split("/")
+            for value in split_line:
+                if value.isdigit() == True:
+                    abe_array[array_index] = split_line[value]
+                    array_index += 1
+                else:
+                    raise TypeError(split_line[value], "can not be ")
+
+    return abe_array
+
                 
 """
 Question 2:
 """
+def printNums(number):
+    if number > 0:
+        print(number)
+        printNums(number - 1)
+
+
+def main():
+    printNums(10)
+
+main()
