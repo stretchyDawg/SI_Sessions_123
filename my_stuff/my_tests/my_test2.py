cb83da92d21ed4972f6500fb3b71cd406f626b82
Christian <cmmorgado53@gmail.com>
2023-03-07 15:19:28 -0500

    Question done

--- my_stuff/my_tests/my_test2.py
+++ my_stuff/my_tests/my_test2.py
@@ -1,4 +1,6 @@
 import array_utils
+import arrays
+import csv
 
 # MAKE UPPERCASE ANSWER:
 def print_file_uppercase(filename):
@@ -42,6 +44,45 @@ def csv_calc(filename):
                     print("Error: INVALID DATA TYPES.")
     except FileNotFoundError as fe:
         print("Error: File \'", filename ,"\' not found.", sep = "")
+
+def array_of_data(filename, test_num):
+    array = arrays.Array(10)
+    index = 0
+    
+    with open(filename) as my_file:
+        next(my_file)
+        csv_reader = csv.reader(my_file)
+        for record in csv_reader:
+            test = record[test_num]
+            if test == "true":
+                array[index] = record[0]
+                index += 1
+
+    return array
+            
+def binary_search(array, target, start=None, end=None):
+    try: 
+        # Input Validation: 
+        if start is None:
+            start = 0 
+            end = len(array)-1
+        if start > end:
+            return None
+
+        midpoint = (start + end) // 2
+        value = array[midpoint]
+
+        if value == target:
+            return midpoint
+        elif value < target:
+            start = midpoint + 1
+            return binary_search(array, target, start, end)
+        elif value > target:
+            end = midpoint - 1
+            return binary_search(array, target, start, end)
+    except TypeError:
+        return None
+
     
 def main():
     array = array_utils.range_array(1, 11)
