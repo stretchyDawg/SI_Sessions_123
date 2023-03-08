907f1d24b6a9e52d30236b8168131cb73fdf2c6b
Christian <cmmorgado53@gmail.com>
2023-03-08 00:36:25 -0500

    fariha2

---
+++ farihas_stuff/str.py
@@ -0,0 +1,12 @@
+def main():
+    fariha = "i like apples"
+    rachel = fariha.replace("apples", "bananas")
+    print("Original:", fariha)
+    print("Replace:", rachel)
+
+    ashton = "Im gonna FART"
+    irwin = ashton.replace("FART", "poop")
+    print("\nOriginal:", ashton)
+    print("Replace:", irwin)
+
+main()
