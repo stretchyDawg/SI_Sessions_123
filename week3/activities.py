665c248fa1e7828e54c537304ca616a626afe191
Christian <cmmorgado53@gmail.com>
2023-09-10 15:19:03 -0400

    Activities

--- week3/activities.py
+++ week3/activities.py
@@ -12,3 +12,45 @@ def circle_area():
     print("Area:", area)
 
 circle_area()
+
+"""
+SHAPE ACTIVITY 2:
+Create a function that prompts a user for the edge of a dodecahedron. Using that edge, calculate the surface area of a dodecahedron and print it to the console. Here is the formula for the surface area of a dodecahedron with ‘a’ being the edge and ‘A’ being the surface area:
+"""
+def dodeca_area():
+    edge = float(input("Enter an edge: "))
+    area = (3)*(25+10*(5**0.5))**0.5
+    area = area*(edge**2)
+    print("Area:", area)
+
+dodeca_area()
+
+"""
+CALCULATOR ACTIVITY (Part 1):
+Create a function that takes two numbers as input and print the result of doing these operations with them (formatting the print statements is not necessary):
+Adding them
+Subtracting them
+Multiplying them
+Dividing them
+Floor dividing them
+Using them as exponents (x^y)
+Print the square root of both of them
+
+(This is very similar to what was done in class but involves more expressions!)
+Part 2 will continue next unit when we learn a new topic (which is a secret for now)!
+"""
+def calc():
+    x = float(input("Enter the first number: "))
+    y = float(input("Enter the second number: "))
+    print("x+y:",x+y)
+    print("x-y:",x-y)
+    print("x*y:",x*y)
+    print("x/y:",x/y)
+    print("x**y:",x**y)
+    print("x//y:",x//y)
+    print("x%y:",x//y)
+    print("sqrt(x):",x**0.5)
+    print("sqrt(y):",y**0.5)
+
+calc()
+
