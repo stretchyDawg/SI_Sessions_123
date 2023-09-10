d638167316ceb9fcb8f84b476aa615b4b2869800
Christian <cmmorgado53@gmail.com>
2023-09-10 17:36:01 -0400

    Added cafe shop activity

--- week3/activities.py
+++ week3/activities.py
@@ -11,7 +11,6 @@ def circle_area():
     area = 3.14*((radius)**2.0)
     print("Area:", area)
 
-circle_area()
 
 """
 SHAPE ACTIVITY 2:
@@ -23,7 +22,34 @@ def dodeca_area():
     area = area*(edge**2)
     print("Area:", area)
 
-dodeca_area()
+
+
+"""
+CAFE SHOP ACTIVITY 
+As the owner of a coffee shop, you wanted a program that quickly calculates the total cost of someone’s order. Write a function (or multiple functions if you want) that will prompt for the number of iced coffees, bagels, and MATCHA (i love matcha)! Then, calculate the total cost of the order. Also, there is a sales tax of 6% for every order, so make sure that the sales tax is in the final price!
+
+These are the prices of each item on the menu:
+Iced Coffee: $4.00
+Bagels: $1.75
+MATCHA: $5.00
+
+Then, print out the final cost of the order!
+"""
+def cafe_shop():
+    iced_coffee = 4.00
+    bagels = 1.75
+    matcha = 5.00
+
+    amount_of_iced_coffee = float(input("How much iced coffees do you want? "))
+    amount_of_bagels = float(input("How much bagels do you want? "))
+    amount_of_matcha = float(input("How much matchas do you want? "))
+
+    total_cost = ((amount_of_bagels)*bagels) + ((amount_of_iced_coffee)*iced_coffee) + ((amount_of_matcha)*matcha)
+    sales_tax = total_cost*.06
+    print("This is the total cost:", total_cost+sales_tax) 
+
+cafe_shop()
+
 
 """
 CALCULATOR ACTIVITY (Part 1):
