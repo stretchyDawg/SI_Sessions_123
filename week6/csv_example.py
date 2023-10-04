8a04eb6a1be1dc033703adbcccd2a8e9f323a002
Christian Morgado <cmm2729@rit.edu>
2023-10-04 14:11:12 -0400

    csv example

---
+++ week6/csv_example.py
@@ -0,0 +1,8 @@
+import csv
+
+def names_and_addresses(csv_filename):
+    with open(csv_filename) as csv_file:
+        csv_reader = csv.reader(csv_file)
+        next(csv_reader)
+        for record in csv_file:
+            print(record[0]) # <-- fields here
