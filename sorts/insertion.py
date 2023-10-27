0981a6b5418c35a0fa6132e0f46c736a67d7076d
Christian <cmmorgado53@gmail.com>
2023-10-27 13:06:40 -0400

    shift w/o hold

--- sorts/insertion.py
+++ sorts/insertion.py
@@ -9,11 +9,12 @@ def shift(a_list, index):
         index -= 1
 
 def shift_wo_swap(a_list, index):
-    target = a_list[index]
-    while index > 0 and a_list[index-1] > a_list[index]:
-        a_list[index] = a_list[index-1]
-        index -= 1
-    a_list[index] = target
+    hold = a_list[index]
+    hold_index = index
+    while hold_index > 0 and hold < a_list[hold_index-1]:
+        a_list[hold_index] = a_list[hold_index-1]
+        hold_index -= 1
+    a_list[hold_index] = hold
     
 def insertion_sort(a_list):
     for index in range(len(a_list)):
@@ -31,6 +32,10 @@ def main():
     
     insertion_sort(test)
     print(test)
+    
+    test2 = [2, 3, 1, 4, 7, 5, 7, 1, 2, 4, 243, 242, 2, 141, 34, 134, 12, 34, 66]
+    insertion_sort_wo_swap(test2)
+    print(test2)
     
 if __name__ == "__main__":
     main()
