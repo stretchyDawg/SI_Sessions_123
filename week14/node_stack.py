c6a974a71c48994cd58887ff6eb531075200c23c
Christian <cmmorgado53@gmail.com>
2023-12-01 13:52:02 -0500

    pancakes

---
+++ week14/node_stack.py
@@ -0,0 +1,101 @@
+import node
+
+class NodeStack:
+    __slots__ = ["__top", "__size"]
+
+    def __init__(self):
+        self.__top = None
+        self.__size = 0
+
+    def get_size(self):
+        return self.__size
+
+    def is_empty(self):
+        return self.__top is None
+
+    def push(self, value):
+        new_top = node.Node(value, self.__top)
+        self.__top = new_top
+        self.__size += 1
+    
+    def peek(self):
+        if self.is_empty():
+            raise IndexError("empty stack")
+        return self.__top.get_value()
+
+    def pop(self):
+        if self.is_empty():
+            raise IndexError("empty stack")
+        else:    
+            popped = self.__top.get_value()
+            self.__top = self.__top.get_next()
+            self.__size -= 1
+            return popped
+
+    def print_stack_comma(self):
+        """
+        Prints node stack in comma (", ") form (for who_goes_there.py __repr__ implementation for crewmate class)
+        """
+        string = ""
+        node = self.__top
+        while node is not None:
+            string += str(node.get_value())
+            node = node.get_next()
+            if node is not None:
+                string += ', '
+        return string
+    
+    def __repr__(self):
+        string = "["
+        node = self.__top
+        while node is not None:
+            string += str(node.get_value())
+            if(node.get_next() != None):
+                string += ' -> '
+            node = node.get_next()
+        string += ']'
+        return string
+    
+def main():
+    stack = NodeStack()
+    print(stack)
+    stack.push(1)
+    print(stack)
+    stack.push(2)
+    stack.push(3)
+    stack.push(4)
+    print(stack)
+    print(stack.peek())
+    stack.pop()
+    print(stack)
+    print(stack.peek())
+    print()
+
+    print()
+    stack2 = NodeStack()
+    print(stack2)
+    stack2.push(1)
+    print(stack2)
+    stack2.push(2)
+    print(stack2)
+    stack2.push(3)
+    print(stack2)
+    stack2.push(4)
+    print(stack2)
+    stack2.push(5)
+    print(stack2)
+    stack2.pop()
+    print(stack2)
+    stack2.pop()
+    print(stack2)
+    stack2.pop()
+    print(stack2)
+    stack2.pop()
+    print(stack2)
+    stack2.pop()
+    print(stack2)
+    print()
+
+if __name__ == "__main__":
+    main()
+    
--- week14/pancakes.py
+++ week14/pancakes.py
@@ -1,3 +1,6 @@
+import random
+import node_stack
+
 class Pancake:
     __slots__ = ["__weight", "__type"]
     
@@ -5,19 +8,118 @@ class Pancake:
         self.__weight = weight
         self.__type = type
 
+    def get_weight(self):
+        return self.__weight
+
+    #representation
     def __str__(self):
-        return "A " + self.__type + " Pancake that weighs " + str(self.__weight) + "lbs."
+        return str(self.__weight) + "lbs "+ self.__type + " Pancake"
     def __repr__(self):
-        return "Pancake\n   Type: " + self.__type +"\n   Weight: " + str(self.__weight) +"lbs" 
+        return "Type: " + self.__type +"\nWeight: " + str(self.__weight) +"lbs\n\n" 
+    
+    #equality
+    def __eq__(self, other):
+        if type(self) == type(other):
+            return (self.__type == other.__type) and (self.__weight == other.__weight)
+        return False
+    def __lt__(self, other):
+        if type(self) == type(other):
+            if (self.__weight == other.__weight):
+                return (self.__type <= other.__type)  # Then sort alphabetically (a simple boolean statement like this does the work for you by comparing ASCII values)
+            return (self.__weight <= other.__weight)  # Sort by weight only if the weight is different
+        return False
+    
+    #other
+    def __hash__(self):
+        return hash(self.__type) + hash(self.__weight)
+    
+    
+    
+    
+    
+class Pancakes:
+    __slots__ = ["__pancakes", "__total_weight"]
+    
+    def __init__(self):
+        self.__pancakes = node_stack.NodeStack()
+        self.__total_weight = 0
+        
+    def add_pancake(self, pancake):
+        self.__pancakes.push(pancake)
+        self.__total_weight += pancake.get_weight()
+        
+    def eat_pancake(self):
+        if not self.__pancakes.is_empty():
+            pancake = self.__pancakes.pop()
+            self.__total_weight -= pancake.get_weight()
+        else:
+            raise IndexError("NO MORE PANCAKES :C")
+        
+    def __lt__(self, other):
+        if type(self) == type(other):
+            return (self.__total_weight <= other.__total_weight)
+        return False
+    
+    def __str__(self):
+        # The round function rounds the number to 1 decimal, you don't need it (and don't use it on your homework)
+        return str(self.__pancakes) +"\nTotal Weight: " + str(round(self.__total_weight, 2)) + "\n"
+        
+        
+        
 
-
-
-
+    
 
 def main():
+    # FOR TESTING, COMMENT OUT THE BLOCKS OF CODE YOU DON'T WANT
+    
     pancake1 = Pancake(2.4, "Chocolate Chip")
     print(pancake1)
     print()
     print(repr(pancake1))
+
+    pan2 = Pancake(1.2, "Banana")
+    pan3 = Pancake(0.2, "Strawberry")
+    pan4 = Pancake(3.6, "Plain")
+    pan5 = Pancake(7.9, "Jumbo")
+    pan6 = Pancake(3.6, "Plain")
+    pan7 = Pancake(0.2, "Jumbo")
+    
+    # The sort() function uses __lt__, so I'm using it to show that our __lt__ and __eq__ methods work:
+    print("\nUnsorted")
+    pancakes = [pancake1, pan2, pan3, pan4, pan5, pan6, pan7]
+    print(repr(pancakes))
+    print("\nSorted:")
+    pancakes.sort()
+    print(repr(pancakes))
+    # I know the formatting looks weird but you should get the idea... it sorts by weight THEN sorts alphabetically
+    
+    # Proof pancakes can be used in a set
+    print("\nSet")
+    set_pancakes = set(pancakes)
+    print(repr(set_pancakes))
+    print()
+    
+    # IMPORTANT TIP:
+    # If you want proof that you need a hash function to put things in sets,
+    # comment out the pancake hash function and run this file again, you'll 
+    # see that you'll get an error when you try to create the set.
+    
+    
+    #------ Pancakes Class ------
+    print("Pancakes Class")
+    pancakes_class = Pancakes()
+    print(pancakes_class)
+    pancakes_class.add_pancake(pancake1)
+    print(pancakes_class)
+    pancakes_class.add_pancake(pan2)
+    print(pancakes_class)
+    pancakes_class.add_pancake(pan3)
+    print(pancakes_class)    
+    pancakes_class.add_pancake(pan4)
+    print(pancakes_class)
+    
+    pancakes_class.eat_pancake()
+    print(pancakes_class)
+
     
 main()
